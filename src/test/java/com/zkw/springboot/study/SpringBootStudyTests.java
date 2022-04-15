8a3132e6bdaae53f236e49efad72a1433f278b55
kevinZ <zhengkaiw851@gmail.com>
2022-04-15 15:04:43 +0800

    unit test

---
+++ src/test/java/com/zkw/springboot/study/SpringBootStudyTests.java
@@ -0,0 +1,16 @@
+package com.zkw.springboot.study;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SpringBootStudyTests {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
